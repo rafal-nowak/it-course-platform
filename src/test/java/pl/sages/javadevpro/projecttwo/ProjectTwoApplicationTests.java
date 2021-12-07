6375672a463fffa8a91fdb45a4ab65aeef32f95c
Piotr Misiopecki <piotr.misiopecki@allegro.pl>
2021-12-07 19:53:48 +0100

    init commit

---
+++ src/test/java/pl/sages/javadevpro/projecttwo/ProjectTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sages.javadevpro.projecttwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
