2814388e6f80191352bcb4055503e4c6515c95ce
Mariusz <czerkiesmariusz@gmail.com>
2021-12-16 20:58:23 +0100

    Add kafkaListener

--- src/main/java/pl/sages/javadevpro/projecttwo/user/UserResource.java
+++ src/main/java/pl/sages/javadevpro/projecttwo/user/UserResource.java
@@ -1,6 +1,7 @@
 package pl.sages.javadevpro.projecttwo.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,4 +21,16 @@ public class UserResource {
         kafkaTemplate.send(TOPIC, message);
         return "Published successfully";
     }
+
+    private void nothing() {
+       // kafkaTemplate.mess
+    }
+
+    @KafkaListener(topics = TOPIC, groupId = "training")
+   // @KafkaListener(topics = TOPIC)
+    public void onMessage(String message) {
+        //log.info("New kafka message: " + message);
+        System.out.println("New kafka message: " + message);
+    }
+
 }
