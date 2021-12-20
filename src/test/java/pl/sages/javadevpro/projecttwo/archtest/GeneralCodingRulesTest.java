8274fcd888fb64fb760b6ab44ba270ac823c8f3b
Anna Saweljew <anna.saweljew@o2.pl>
2021-12-20 19:06:11 +0100

    architecture tests with ArchUnit

---
+++ src/test/java/pl/sages/javadevpro/projecttwo/archtest/GeneralCodingRulesTest.java
@@ -0,0 +1,17 @@
+package pl.sages.javadevpro.projecttwo.archtest;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.CompositeArchRule;
+import com.tngtech.archunit.library.GeneralCodingRules;
+
+@AnalyzeClasses(packages = "pl.sages.javadevpro.projecttwo")
+public class GeneralCodingRulesTest {
+
+    @ArchTest
+    public static final ArchRule generalRules = CompositeArchRule.of(
+            GeneralCodingRules.NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS)
+            .and(GeneralCodingRules.NO_CLASSES_SHOULD_USE_FIELD_INJECTION)
+            .and(GeneralCodingRules.NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS);
+}
