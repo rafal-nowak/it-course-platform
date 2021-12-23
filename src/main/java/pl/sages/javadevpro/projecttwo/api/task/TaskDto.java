c8f41a7b2d44c29a51db74245a040360b359ee6c
Mariusz <czerkiesmariusz@gmail.com>
2021-12-23 21:31:16 +0100

    ADD TaskEndpoint more

--- src/main/java/pl/sages/javadevpro/projecttwo/api/task/TaskDto.java
+++ src/main/java/pl/sages/javadevpro/projecttwo/api/task/TaskDto.java
@@ -1,8 +1,14 @@
 package pl.sages.javadevpro.projecttwo.api.task;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class TaskDto {
 
     private Integer id;
