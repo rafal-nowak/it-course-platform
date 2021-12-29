b6077fc9105f18c61e06451f70da172c018eaf5d
Anna Saweljew <anna.saweljew@o2.pl>
2021-12-29 18:30:15 +0100

    refactor mappers with mapstruct

--- src/main/java/pl/sages/javadevpro/projecttwo/api/user/UserDtoMapper.java
+++ src/main/java/pl/sages/javadevpro/projecttwo/api/user/UserDtoMapper.java
@@ -1,31 +1,15 @@
 package pl.sages.javadevpro.projecttwo.api.user;
 
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import pl.sages.javadevpro.projecttwo.domain.user.User;
-import pl.sages.javadevpro.projecttwo.domain.user.UserRole;
 
-@Component
-public class UserDtoMapper {
+@Mapper
+public interface UserDtoMapper {
 
-    public UserDto toDto(User user) {
-        if (user != null) {
-            return new UserDto(
-                user.getEmail(),
-                user.getName(),
-                "######",
-                user.getRoles()
-            );
-        }
+    @Mapping(target="password", constant = "######")
+    UserDto toDto(User user);
 
-        return null;
-    }
+    User toDomain(UserDto userDto);
 
-    public User toDomain(UserDto dto) {
-        return new User(
-            dto.getEmail(),
-            dto.getName(),
-            dto.getPassword(),
-            dto.getRoles()
-        );
-    }
 }
