c096934a40589aa1504987e05ed53fc48652c6ca
Anna Saweljew <anna.saweljew@o2.pl>
2022-01-19 21:52:08 +0100

    add get result endpoint

--- src/main/java/pl/sages/javadevpro/projecttwo/api/UserTaskEndpoint.java
+++ src/main/java/pl/sages/javadevpro/projecttwo/api/UserTaskEndpoint.java
@@ -6,18 +6,22 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 import pl.sages.javadevpro.projecttwo.api.usertask.AssignTaskRequest;
 import pl.sages.javadevpro.projecttwo.api.usertask.MessageResponse;
+import pl.sages.javadevpro.projecttwo.api.usertask.UserTaskDto;
+import pl.sages.javadevpro.projecttwo.api.usertask.UserTaskDtoMapper;
 import pl.sages.javadevpro.projecttwo.domain.UserTaskService;
-
+import pl.sages.javadevpro.projecttwo.domain.usertask.UserTask;
 
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(path = "/usertask/assign")
 public class UserTaskEndpoint {
 
     private final UserTaskService userTaskService;
+    private final UserTaskDtoMapper mapper;
+
 
     @PostMapping(
+            path = "/usertask/assign",
             produces = "application/json",
             consumes = "application/json"
     )
@@ -25,5 +29,17 @@ public class UserTaskEndpoint {
     public ResponseEntity<MessageResponse> assignTaskToUser(@RequestBody AssignTaskRequest assignTaskRequest) {
         userTaskService.assignTask(assignTaskRequest.getUserEmail(), assignTaskRequest.getTaskId());
         return ResponseEntity.ok(new MessageResponse("OK", "Task assigned to user"));
+    }
+
+
+    @GetMapping(
+            path = "/users/{email}/tasks/{id}",
+            produces = "application/json",
+            consumes = "application/json"
+    )
+    @Secured("ROLE_USER") //TODO protect user from seeing other users results (or details)
+    public ResponseEntity<UserTaskDto> getUserTaskResult(@PathVariable String email, @PathVariable String id){
+        UserTask userTask = userTaskService.getUserTask(email,id);
+        return ResponseEntity.ok(mapper.toDto(userTask));
     }
 }
--- src/main/java/pl/sages/javadevpro/projecttwo/api/usertask/UserTaskDtoMapper.java
+++ src/main/java/pl/sages/javadevpro/projecttwo/api/usertask/UserTaskDtoMapper.java
@@ -1,6 +1,7 @@
 package pl.sages.javadevpro.projecttwo.api.usertask;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import pl.sages.javadevpro.projecttwo.domain.usertask.UserTask;
 
 @Mapper
