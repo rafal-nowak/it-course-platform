22499f93f3e07207db9e3d3fc86a5c6a63afb780
Anna Saweljew <anna.saweljew@o2.pl>
2022-02-01 19:34:46 +0100

    unlink remote when clone repository

--- src/main/java/pl/sages/javadevpro/projecttwo/external/git/JGitService.java
+++ src/main/java/pl/sages/javadevpro/projecttwo/external/git/JGitService.java
@@ -2,10 +2,12 @@ package pl.sages.javadevpro.projecttwo.external.git;
 
 import lombok.SneakyThrows;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.springframework.stereotype.Component;
 import pl.sages.javadevpro.projecttwo.domain.usertask.GitService;
 
 import java.io.File;
+import java.util.Set;
 
 @Component
 public class JGitService implements GitService {
@@ -18,8 +20,22 @@ public class JGitService implements GitService {
                 .setURI(repositoryPath)
                 .setDirectory(repo)
                 .call();
+        unlinkRemotes(git);
         git.close();
         return git.getRepository().getDirectory();
     }
+
+
+    private void unlinkRemotes(Git git) {
+        Set<String> remoteNames = git.getRepository().getRemoteNames();
+        remoteNames.forEach(g -> {
+            try {
+                git.remoteRemove().setRemoteName(g).call();
+            } catch (GitAPIException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
 
 }
