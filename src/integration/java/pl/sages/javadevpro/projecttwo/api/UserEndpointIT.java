3bd1fd475c1f28721599144987f3251d74870566
rafal-nowak <contact@rafal-nowak.com>
2022-02-04 21:27:30 +0100

    cleaned unused imports

--- src/integration/java/pl/sages/javadevpro/projecttwo/api/UserEndpointIT.java
+++ src/integration/java/pl/sages/javadevpro/projecttwo/api/UserEndpointIT.java
@@ -1,6 +1,5 @@
 package pl.sages.javadevpro.projecttwo.api;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
--- src/main/java/pl/sages/javadevpro/projecttwo/config/SecurityConfiguration.java
+++ src/main/java/pl/sages/javadevpro/projecttwo/config/SecurityConfiguration.java
@@ -1,9 +1,7 @@
 package pl.sages.javadevpro.projecttwo.config;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
