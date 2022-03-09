bbcbff7a6e39ef479724775f694e5f4f815a0585
Marcin Kowalski <raggy2k4@gmail.com>
2022-03-09 20:47:53 +0100

    refactor - add writeFile() in WorkspaceService

--- src/main/java/pl/sages/javadevpro/projecttwo/external/workspace/WorkspaceService.java
+++ src/main/java/pl/sages/javadevpro/projecttwo/external/workspace/WorkspaceService.java
@@ -14,6 +14,7 @@ import pl.sages.javadevpro.projecttwo.external.directory.task.FileToBeDeliveredT
 import pl.sages.javadevpro.projecttwo.external.directory.task.TaskDefinition;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
@@ -69,7 +70,17 @@ public class WorkspaceService implements Workspace {
 
     @Override
     public void writeFile(String rootPathUrl, String path, byte[] bytes) {
+        String fullPath = rootPathUrl + path;
+        File file = new File(fullPath);
 
+        try {
+            file.createNewFile();
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(bytes);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -94,5 +105,20 @@ public class WorkspaceService implements Workspace {
             }
         });
     }
+
+    public File takeFileFromUserTask(String userId, String taskId, String fileId) {
+        List<String> listOfAvailableFilesForUserTask = readListOfAvailableFilesForUserTask(userId, taskId);
+
+        if (Integer.parseInt(fileId) > listOfAvailableFilesForUserTask.size()) {
+            throw new ResourceNotFoundException("File " + fileId + " was not assigned to " + taskId + " user task " + userId);
+        }
+
+        String relatedPathToSelectedFile = listOfAvailableFilesForUserTask.get(Integer.parseInt(fileId) - 1);
+
+        String path = getPathToUserTask(userId, taskId) + "/" + relatedPathToSelectedFile;
+
+        return new File(path);
+    }
+
 
 }
