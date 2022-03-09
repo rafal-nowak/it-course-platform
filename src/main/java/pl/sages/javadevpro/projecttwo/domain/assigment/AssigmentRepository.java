e2c110c92f4dcf58a5e5f8a1abdbce2a47a71bb3
landrzejewski <landrzejewski.poczta@gmail.com>
2022-03-09 17:46:40 +0100

    UserTask

--- src/main/java/pl/sages/javadevpro/projecttwo/domain/assigment/AssigmentRepository.java
+++ src/main/java/pl/sages/javadevpro/projecttwo/domain/assigment/AssigmentRepository.java
@@ -1,10 +1,8 @@
 package pl.sages.javadevpro.projecttwo.domain.assigment;
 
-import java.util.Optional;
-
 public interface AssigmentRepository {
 
-//    Optional<Assigment> find(String userId, String taskId); TODO
+    //    Optional<Assigment> find(String userId, String taskId); TODO
 
     Assigment save(Assigment assigment);
 
