1c813218bdd398a4b0d3e4b9e2611bb107621881
Piotr Misiopecki <piotr.misiopecki@allegro.pl>
2022-04-01 21:11:49 +0200

    refactor TaskDefinition

--- src/main/java/pl/sages/javadevpro/projecttwo/external/directory/task/TaskDefinition.java
+++ src/main/java/pl/sages/javadevpro/projecttwo/external/directory/task/TaskDefinition.java
@@ -4,16 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
+import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class TaskDefinition {
-    //todo 13. Set zamiast listy
-    private List<FileToBeDeliveredToUser> filesToBeDeliveredToUser;
+
+    private Set<FileToBeDeliveredToUser> filesToBeDeliveredToUser;
     private Environment environment;
     private String taskVerificationMethod;
-    //todo 14. Set zamiast listy
-    private List<ResultFile> resultFiles;
+    private Set<ResultFile> resultFiles;
 }
