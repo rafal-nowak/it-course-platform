a4d60965c40781ea43038dbb613f3c0386b28b42
Mariusz <czerkiesmariusz@gmail.com>
2022-04-21 20:23:13 +0200

    todo -> Mariusz exception

--- src/main/java/pl/sages/javadevpro/projecttwo/api/task/TaskController.java
+++ src/main/java/pl/sages/javadevpro/projecttwo/api/task/TaskController.java
@@ -7,15 +7,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import pl.sages.javadevpro.projecttwo.api.task.dto.CommandName;
 import pl.sages.javadevpro.projecttwo.api.task.dto.TaskControllerCommand;
 import pl.sages.javadevpro.projecttwo.api.task.verification.VerifyTaskAuthorization;
 import pl.sages.javadevpro.projecttwo.api.usertask.ListOfFilesResponse;
 import pl.sages.javadevpro.projecttwo.api.usertask.MessageResponse;
+import pl.sages.javadevpro.projecttwo.domain.task.CommitTaskException;
 import pl.sages.javadevpro.projecttwo.domain.task.IncorrectTaskStatusException;
 import pl.sages.javadevpro.projecttwo.domain.task.TaskService;
 import pl.sages.javadevpro.projecttwo.domain.task.TaskStatus;
 
+import java.io.IOException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -73,12 +76,16 @@ public class TaskController {
     public ResponseEntity<MessageResponse> postFileAssignedToUserTask(
             @PathVariable String taskId,
             @PathVariable int fileId,
-            @RequestParam("bytes") byte[] bytes
+            @RequestParam("file") MultipartFile file
     ) {
         if (taskService.getTaskStatus(taskId).equals(TaskStatus.SUBMITTED)) {
             throw new IncorrectTaskStatusException();
         }
-        taskService.writeAndCommitTask(taskId, fileId, bytes);
+        try {
+            taskService.writeAndCommitTask(taskId, fileId, file.getBytes());
+        } catch (IOException e) {
+            throw new CommitTaskException();
+        }
         return ResponseEntity.ok(new MessageResponse("The File Uploaded Successfully"));
     }
 
